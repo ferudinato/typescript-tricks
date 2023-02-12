cad130e1367a2670d578ecd4376093979e44f6e6
Ferdroid8080 <andresf@ferudinato.com>
2023-02-12 18:44:34 -0500

    [add] workshop 10: unknown type

---
+++ 10-avoid-errors-unknown.ts
@@ -0,0 +1,14 @@
+type Some = {
+	something: () => void;
+};
+
+function log(val: unknown) {
+	if (typeof val === 'string') {
+		console.log(val.toUpperCase());
+	}
+
+	console.log((val as number) + 20);
+	console.log((val as Some).something());
+}
+
+log(5);
